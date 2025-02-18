aecb88a82dff70472e14ceb8f9c7099f8ec7666e
Jay Ganbat <javkhaa@live.com>
2025-02-18 10:02:41 +0000

    patch error

--- bin/image_inference.py
+++ bin/image_inference.py
@@ -2,10 +2,36 @@ import glob
 import logging
 import os
 import click
-from utility.image_prediction import get_initial_prediction
+from ultralytics import YOLO
+from utility.image_prediction import get_result_coordinates, get_initial_prediction
+import shutil
+from PIL import Image
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+# TODO FIX THIS and merge with get_initial_prediction in image_prediction.py
+def write_initial_predictions(images, model_path, out_dir, conf=0.5):
+    os.makedirs(os.path.join(out_dir, "obj_train_data"), exist_ok=True)
+    obj_inference_dir = os.path.join(out_dir, "obj_train_data")
+    model = YOLO(model_path)
+    # Run batched inference on a list of images
+    results = model.predict(images, conf=conf)  # return a list of Results objects
+
+    bbox_coordinates = get_result_coordinates(results, use_normalized_coordinates=True)
+
+    # Process results list
+    for img_file, bbox_coord in zip(images, bbox_coordinates):
+        # Copy the image file to the obj_train_data directory
+        shutil.copy(img_file, obj_inference_dir)
+        basename = os.path.basename(img_file)
+        with open(
+            os.path.join(obj_inference_dir, f"{basename.removesuffix('.png')}.txt"), "w"
+        ) as fp:
+            for bbox in bbox_coord:
+                line = " ".join([str(x) for x in bbox])
+                fp.write(f"0 {line}\n")
 
 
 @click.command()
@@ -13,7 +39,7 @@ logger = logging.getLogger(__name__)
 @click.option(
     "--out_dir",
     "-o",
-    type=str,
+    type=click.Path(exists=True),
     help="Output dir where CVAT importable annotations will be saved.",
 )
 @click.option("--model_path", "-m", type=click.Path(exists=True))
@@ -48,7 +74,7 @@ def create_cvat_importable_annotations(image_dir, out_dir, model_path, conf=0.5)
         for image_file in images:
             fp.write(f"data/obj_train_data/{os.path.basename(image_file)}\n")
 
-    get_initial_prediction(
+    write_initial_predictions(
         images=images, model_path=model_path, out_dir=out_dir, conf=conf
     )
 
